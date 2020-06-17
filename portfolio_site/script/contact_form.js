27c5366fc68822badc9d6a10d0301c3be8294cd1
Robin S Bartlett Larsen <hundset@gmail.com>
2020-06-17 14:46:54 +0200

    Added simple validation of contact form inputs, contact form is still not functional, just an update on the browser side.

--- portfolio_site/script/contact_form.js
+++ portfolio_site/script/contact_form.js
@@ -1,1 +1,42 @@
 
+
+function validateName() {
+
+var name = document.getElementById('name');
+var nameSpan = document.getElementById('nameError');
+
+    if (name.value.trim() == "") {
+        nameSpan.style.display = "block";
+        return false;
+    } else {
+        nameSpan.style.display = "none";
+    }
+}
+
+
+function validateMessage() {
+
+var message = document.getElementById('subject');
+var messageSpan = document.getElementById('messageError');  
+
+    if (message.value.trim() == "") {
+        messageSpan.style.display = "block";
+        return false;
+    } else {
+        messageSpan.style.display = "none";
+    }
+}
+
+
+function validateEmail() {
+
+var email = document.getElementById('email');
+var emailSpan = document.getElementById('emailError');
+
+    if (/[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email.value)) {
+        emailSpan.style.display = "none";
+        return true;
+    } else {
+        emailSpan.style.display = "block";
+    }
+}
